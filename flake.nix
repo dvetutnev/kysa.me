01be7ab324302820287b05af7ca969bcc5d5e8e4
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-01-21 21:07:08 +0300

    minimal preview web-server with simple index.html

--- flake.nix
+++ flake.nix
@@ -7,25 +7,36 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      index_html = system:
+        nixpkgs.legacyPackages.${system}.writeText "index.html"
+          ''
+          <!DOCTYPE html>
+          <html>
+          <body>
+          <p>Hello world!</p>
+          </body>
+          </html>
+          '';
     in
     {
       packages = forAllSystems (system:
         {
-          default = nixpkgs.legacyPackages.${system}.writeTextDir "index.html"
-          ''
-          <html>
-          </html>
-          '';
+          default = nixpkgs.legacyPackages.${system}.linkFarmFromDrvs "www_root"
+            [
+              (index_html system)
+            ];
         });
 
       apps = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          www_root = self.packages.${system}.default;
           previewServer = pkgs.writeShellApplication
             {
               name = "server";
               runtimeInputs = [ pkgs.caddy ];
-              text = "caddy --version";
+              text = "caddy file-server --listen 127.0.0.1:8080 --root ${www_root}";
             };
         in
         {
