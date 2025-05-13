8d84f3cb59f8daa31c02c7b8bf6b57ed4d68a77e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-05-13 17:04:35 +0300

    cssArgs

--- flake.nix
+++ flake.nix
@@ -5,84 +5,79 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-
       _mkSite = { runCommand, writeTextDir, symlinkJoin, pandoc, lib }:
-        url:
+        siteUrl:
+
         let
-          index_html = writeTextDir "index.html"
-          ''
-          <!DOCTYPE html>
-          <html>
-          <body>
-          <p>Hello world!</p>
-          </body>
-          </html>
+          index_html = writeTextDir "index.html" ''
+            <!DOCTYPE html>
+            <html>
+            <body>
+            <p>Hello world!</p>
+            </body>
+            </html>
           '';
 
-	  addFile = file: writeTextDir (lib.path.removePrefix ./. file)
+          addFile = file:
+            writeTextDir (lib.path.removePrefix ./. file)
             (builtins.readFile file);
 
-	  page = file:
+          page = file:
             let
-	      name = lib.path.removePrefix ./. file;
-	      makeCSSArg = css: "--css=${url + (lib.path.removePrefix ./. css)}";
-	      css = makeCSSArg ./css/hyde.css;
+              removeCurrentDirPrefix = filePath:
+                lib.strings.removePrefix "./"
+                (lib.path.removePrefix ./. filePath);
+              name = removeCurrentDirPrefix file;
+              makeCSSArg = cssFile:
+                "--css=${siteUrl + (removeCurrentDirPrefix cssFile)}";
+              cssArgs = lib.concatStringsSep " "
+                (map makeCSSArg [ ./css/pole.css ./css/hyde.css ]);
 
-	    in
-              runCommand name {} ''
-	      target=$out/${lib.escapeShellArg name}.html
+            in runCommand name { } ''
+              target=$out/${lib.escapeShellArg name}.html
               mkdir -p "$(dirname "$target")"
-	      ls -la
-echo ">>>>>taRGET"
-echo "$target"
-echo "wrrrrrite file"
-echo "dddddd" > "$target"
-echo "${file}"
-echo "${css}"
+              echo ">>>>>taRGET"
+              echo "$target"
+              echo "wrrrrrite file"
+              echo "dddddd" > "$target"
+              echo "${file}"
+              echo "${cssArgs}"
               ${lib.getExe pandoc} --version 
-	      '';
-        in
-          symlinkJoin {
-            name = "www_root";
-            paths = [
-              index_html
-	      (page ./README.md)
-            ] ++ map (p: addFile p) [
-	      ./css/poole.css
-	      ./css/syntax.css
-	      ./css/hyde.css
-	      ./css/hyde-styx.css
-	    ];
+            '';
+        in symlinkJoin {
+          name = "www_root";
+          paths = [ index_html (page ./README.md) ] ++ map (p: addFile p) [
+            ./css/poole.css
+            ./css/syntax.css
+            ./css/hyde.css
+            ./css/hyde-styx.css
+          ];
 
-          };
+        };
 
-    in
-      {
-        packages = forAllSystems (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            mkSite = pkgs.callPackage _mkSite {};
-          in
-            {
-              site = mkSite "http://localhost:8080/";
-            });
-        
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          mkSite = pkgs.callPackage _mkSite { };
+        in { site = mkSite "http://localhost:8080/"; });
+
       apps = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          site = pkgs.callPackage _mkSite {};
+          site = pkgs.callPackage _mkSite { };
           www_root = site "http://localhost:8080/";
-          previewServer = pkgs.writeShellApplication
-            {
-              name = "server";
-              runtimeInputs = [ pkgs.caddy ];
-              text = "caddy file-server --listen 127.0.0.1:8080 --root ${www_root}";
-            };
-        in
-        {
+          previewServer = pkgs.writeShellApplication {
+            name = "server";
+            runtimeInputs = [ pkgs.caddy ];
+            text =
+              "caddy file-server --listen 127.0.0.1:8080 --root ${www_root}";
+          };
+        in {
           default = {
             type = "app";
             program = nixpkgs.lib.getExe previewServer;
