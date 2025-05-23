519ba7bc0dab3a35b25b6e0f9395695bee16ffeb
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-05-23 22:30:35 +0300

    separate css

--- site.nix
+++ site.nix
@@ -9,43 +9,20 @@
 siteUrl:
 
 let
-  css = [
-    ./css/poole.css
-    ./css/syntax.css
-    ./css/hyde.css
-    ./css/hyde-styx.css
-    "https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700|Abril+Fatface"
-  ];
 
   removeCurrentDirPrefix = import ./rm_cur_dir_prefix.nix { inherit lib; };
   addFile = import ./add_file.nix { inherit stdenv removeCurrentDirPrefix; };
   mkSideBar = import ./mk_side_bar.nix { inherit lib; };
+  mkCSS = import ./mk_css.nix { inherit lib removeCurrentDirPrefix; };
+  css = mkCSS siteUrl;
 
   mkHTML =
     file:
     let
       template = ./default.html5;
-
       include_before = mkSideBar siteUrl;
-
       name = builtins.replaceStrings [ ".md" ] [ ".html" ] (removeCurrentDirPrefix file);
-
       drvName = builtins.replaceStrings [ "/" ] [ "-" ] name;
-
-      mkCSSArg =
-        cssPath:
-        let
-          res =
-            if
-              builtins.isPath cssPath # \
-            then
-              siteUrl + (removeCurrentDirPrefix cssPath)
-            else
-              cssPath;
-        in
-        lib.escapeShellArg "--css=${res}";
-
-      cssArgs = lib.concatStringsSep " " (map mkCSSArg css);
 
     in
     runCommand drvName
@@ -60,7 +37,7 @@ let
                              --template=${template} \
                              --to=html5 \
                              --output="$target" \
-                             ${cssArgs} \
+                             ${css.args} \
                              --variable=include-before:${lib.escapeShellArg include_before} \
                              ${file} \
                              --verbose
@@ -91,5 +68,5 @@ symlinkJoin {
     (mkHTML ./pages/about.md)
     (addFile ./dir/nix_hacking_1.png)
     (addFile ./you_are_here.png)
-  ] ++ map (p: addFile p) (builtins.filter (x: builtins.isPath x) css);
+  ] ++ map (p: addFile p) css.paths;
 }
