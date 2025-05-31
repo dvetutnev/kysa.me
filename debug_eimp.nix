9b5ab9a2ad5a005b1586c6703446074ca0cdaa74
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-05-31 21:09:14 +0300

    refactoring test collectImages

--- debug_eimp.nix
+++ debug_eimp.nix
@@ -4,70 +4,65 @@
 
 let
   runTests = pkgs.lib.runTests;
-  traceVal = pkgs.lib.traceVal;
-in
-runTests {
-  test2 =
-    let
-      blocks = [
-        {
-          t = "Image";
-          c = "pic1.jpg";
-        }
-        {
-          t = "Block";
-          c = [
-            {
-              t = "Image";
-              c = "pic2.jpg";
-            }
-            {
-              t = "Header";
-              c = "tEEExt";
-            }
-            {
-              t = "Image";
-              c = "pic3.jpg";
-            }
-            {
-              t = "Block";
-              c = [
-                {
-                  t = "Text";
-                  c = "text";
-                }
-                {
-                  t = "Image";
-                  c = "pic4.jpg";
-                }
-              ];
-            }
-          ];
-        }
-      ];
+  collectImages = import ./collect_images.nix;
 
-      collectImages = import ./collect_images.nix;
-
-    in
+  blocks = [
     {
-      expr = collectImages blocks;
-      expected = [
-        {
-          t = "Image";
-          c = "pic1.jpg";
-        }
+      t = "Image";
+      c = "pic1.jpg";
+    }
+    {
+      t = "Block";
+      c = [
         {
           t = "Image";
           c = "pic2.jpg";
+        }
+        {
+          t = "Header";
+          c = "tEEExt";
         }
         {
           t = "Image";
           c = "pic3.jpg";
         }
         {
-          t = "Image";
-          c = "pic4.jpg";
+          t = "Block";
+          c = [
+            {
+              t = "Text";
+              c = "text";
+            }
+            {
+              t = "Image";
+              c = "pic4.jpg";
+            }
+          ];
         }
       ];
-    };
+    }
+  ];
+in
+runTests {
+  test2 = {
+    expr = collectImages blocks;
+    expected = [
+      {
+        t = "Image";
+        c = "pic1.jpg";
+      }
+      {
+        t = "Image";
+        c = "pic2.jpg";
+      }
+      {
+        t = "Image";
+        c = "pic3.jpg";
+      }
+      {
+        t = "Image";
+        c = "pic4.jpg";
+      }
+    ];
+  };
 }
