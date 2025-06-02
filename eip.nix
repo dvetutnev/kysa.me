5b69ef65ba129826596430bb2bb49811ce47c677
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-02 13:23:51 +0300

    extract image path arg

--- eip.nix
+++ eip.nix
@@ -2,11 +2,12 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+file:
 let
   md2ast = import ./md2ast.nix { };
   collectImages = import ./collect_images;
 
-  ast = md2ast ./t.md;
+  ast = md2ast file;
   images = collectImages ast.blocks;
   paths = with builtins; map (e: head (elemAt e.c 2)) images;
 in
