fb652506d16e55d872362a51f69d117d7ea73ff2
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-03 13:25:59 +0300

    try use pandoc-plantuml filter

--- mk_html.nix
+++ mk_html.nix
@@ -41,18 +41,35 @@ runCommand drvName
   {
     preferLocalBuild = true;
     allowSubstitutes = false;
-    nativeBuildInputs = [ plantuml ];
+    nativeBuildInputs = [
+      plantuml
+      pandoc-plantuml-filter
+    ];
   }
   ''
-    target=$out/${lib.escapeShellArg name}
-    mkdir -p "$(dirname "$target")"
-    ${lib.getExe pandoc} --standalone \
-                         --template=${template} \
-                         --to=html5 \
-                         --output="$target" \
-                         ${cssArgs} \
-                         --variable=include-before:${lib.escapeShellArg sideBar} \
-                         --title-prefix=${lib.escapeShellArg titlePrefix} \
-                         ${file} \
-                         --verbose
+     target=$out/${lib.escapeShellArg name}
+     mkdir -p "$(dirname "$target")"
+     mkdir d
+     ${lib.getExe pandoc} --standalone \
+                          --template=${template} \
+                          --to=html5 \
+                          --output="$target" \
+                          ${cssArgs} \
+                          --variable=include-before:${lib.escapeShellArg sideBar} \
+                          --title-prefix=${lib.escapeShellArg titlePrefix} \
+                          ${file} \
+                          --filter pandoc-plantuml \
+                          --extract-media=d \
+                          --verbose
+    echo "$PWD"
+    ls -la
+    ls -la /build/d/
+    if [ -d "/build/d/plantuml-images" ]; then
+       echo "Exists plantuml-images"
+       ls -la /build/d/plantuml-images/
+    else
+      echo "No plantuml-images"
+    fi
+
+    echo 21
   ''
