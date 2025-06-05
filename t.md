056aaf738722f1d5f6de6b72ceddda48188d07c3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-05 14:18:43 +0300

    clean

--- t.md
+++
@@ -1,9 +0,0 @@
-![pic](dir/nested/pic.png)
-
-![](pic.jpg)
-
-[About](pages/about.md "Titlr of link")
-
-[Global](https://ya.ru)
-
-[Global2](http://ya.ru)
--- test2.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-with pkgs;
-testers.testEqualContents {
-  assertion = "describe of test_";
-  expected = writeText "expected" "text423";
-  actual = writeText "actual" "text423";
-}
