8822eaf342a4530b8f474d6e6c71979216c30910
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-05 12:01:41 +0300

    refactoring mk_page, move html back

--- mk_page.nix
+++ mk_page.nix
@@ -6,6 +6,9 @@
   runCommand,
   pandoc,
   symlinkJoin,
+  plantuml,
+  pandoc-plantuml-filter,
+  makeFontsConf,
 
   lib,
 }:
@@ -14,28 +17,77 @@
   css,
   siteUrl,
   sideBar,
+  titlePrefix ? "kysa.me",
+  lang ? "ru-RU",
 }:
 
 file:
 let
-  mkHTML =
-    callPackage ./mk_html.nix
+  html =
+    let
+      template = ./default.html5;
+      relative2absolute-links = ./relative2absolute-links.lua;
+
+      name = builtins.replaceStrings [ ".md" ] [ ".html" ] (removeCurrentDirPrefix file);
+      drvName = builtins.replaceStrings [ "/" ] [ "-" ] name;
+
+      mkCmdArg =
+        cssPath:
+        let
+          res =
+            if
+              builtins.isPath cssPath # \
+            then
+              siteUrl + (removeCurrentDirPrefix cssPath)
+            else
+              cssPath;
+        in
+        lib.escapeShellArg "--css=${res}";
+      cssArgs = lib.concatStringsSep " " (map mkCmdArg css);
+    in
+    runCommand drvName
       {
-        inherit removeCurrentDirPrefix;
+        preferLocalBuild = true;
+        allowSubstitutes = false;
+        nativeBuildInputs = [
+          plantuml
+          pandoc-plantuml-filter
+        ];
+
+        FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ ]; };
+        SITE_URL = siteUrl;
       }
-      {
-        inherit css siteUrl sideBar;
-      };
+      ''
+         target=$out/${lib.escapeShellArg name}
+         mkdir -p "$(dirname "$target")"
+         HOME="$(mktemp -d)" # for fontconfig
+
+         ${lib.getExe pandoc} --standalone \
+                              --template=${template} \
+                              --to=html5 \
+                              --output="$target" \
+                              ${cssArgs} \
+                              --variable=include-before:${lib.escapeShellArg sideBar} \
+                              --title-prefix=${lib.escapeShellArg titlePrefix} \
+                              --metadata=lang:${lang} \
+                              --lua-filter=${relative2absolute-links} \
+                              --filter pandoc-plantuml \
+                              --verbose \
+                              ${file}
+
+        if [ -d "plantuml-images" ]; then
+           echo "Install plantuml images"
+           find plantuml-images -type d -exec install -d -m 755 {} $out/{} \;
+           find plantuml-images -type f -name '*.png' -exec install -m 644 {} $out/{} \;
+        fi
+      '';
 
   extractImagePaths = callPackage ./extract_image_paths.nix { };
-
-  paths = extractImagePaths file;
+  imagePaths = extractImagePaths file;
 in
 symlinkJoin {
   name = builtins.toString file;
-  paths =
-    [
-      (mkHTML file)
-    ] # /
-    ++ map (p: addFile (./. + "/${p}")) paths;
+  paths = [
+    html
+  ] ++ map (p: addFile (./. + "/${p}")) imagePaths;
 }
