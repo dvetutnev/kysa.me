fb7391a4068c0fdea3b0f11114e5be872d578c14
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-07 22:25:54 +0300

    refactoring mkSite

--- site2.nix
+++ site2.nix
@@ -16,16 +16,16 @@ let
     ./css/hyde-styx.css
     "https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700|Abril+Fatface"
   ];
+
+  cssDrvs = map (
+    p:
+    addFile {
+      path = p;
+      prefix = ./.;
+    }
+  ) (builtins.filter (x: builtins.isPath x) css);
 in
 symlinkJoin {
   name = "www_root";
-  paths =
-    [ ]
-    ++ map (
-      p:
-      addFile {
-        path = p;
-        prefix = ./.;
-      }
-    ) (builtins.filter (x: builtins.isPath x) css);
+  paths = [ ] ++ cssDrvs;
 }
