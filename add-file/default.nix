486ca07bf3a66c43d08265a405ae36d42a02bc02
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-11 13:09:25 +0300

    simplification addFile

--- add-file/default.nix
+++ add-file/default.nix
@@ -7,7 +7,7 @@
 { path, prefix }:
 stdenv.mkDerivation rec {
   destName = stripPrefix { inherit path prefix; };
-  name = lib.strings.replaceStrings [ "/" ] [ "-" ] destName;
+  name = builtins.toString destName;
   buildCommand = ''
     install -m 644 -D ${path} $out/${destName}
   '';
